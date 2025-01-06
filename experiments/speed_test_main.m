b84e2ec1d07b49e482998bada4351e2f1d7cf9aa
zbb-cloud <1341020780@qq.com>
2025-01-07 03:33:45 +0800

    Update speed_test_main.m zbb

--- experiments/speed_test_main.m
+++ experiments/speed_test_main.m
@@ -3,9 +3,9 @@ close all
 clc
 
 time_today = datestr(now, 'mmddyyyy');
-
+%设置轨迹类型为 'circle'，并调用 speed_test 函数
 traj_type = 'circle';
 speed_test
-
+%设置轨迹类型为 'infty'，并再次调用 speed_test 函数
 traj_type = 'infty';
 speed_test
